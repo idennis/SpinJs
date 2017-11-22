18a367f0e8bf80b79f1c9a668f0c1121ad4313d5
Dennis <dennishou132@gmail.com>
2017-11-23 02:49:37 +1100

    More basic functions

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 window.spinny = (function () {
-    function Spinny (els) {
 
+    function Spinny (els) {
+        for (var i = 0; i < els.length; i++ ) {
+               this[i] = els[i];
+           }
+           this.length = els.length;
     }
 
+    Spinny.prototype.map = function (callback) {
+        var results = [], i = 0;
+        for ( ; i < this.length; i++) {
+            results.push(callback.call(this, this[i], i));
+        }
+        return results;
+    };
+
+    Spinny.prototype.forEach(callback) {
+        this.map(callback);
+        return this;
+    };
     var spinny = {
         get: function (selector) {
             var els;
